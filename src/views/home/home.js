2ca1b5b63639cfa0ba6bc5a6da467d9177761ad2
Muneeb Naveed <personal.muneebnaveed@gmail.com>
2024-03-03 22:53:08 +0100

    componentize services

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -5,6 +5,7 @@ import ProjectTile from "./project-tile";
 import Footer from "../../components/layout/footer";
 import CtaButton, { CtaButtonVariants } from "@/components/cta-button";
 import Portfolio from "./portfolio";
+import Services from "./services";
 
 const HomeView = () => {
   return (
@@ -125,86 +126,7 @@ const HomeView = () => {
         </div>
       </section>
       <Portfolio />
-      <section className="py-[86px] px-10 relative">
-        <div className="absolute top-0 left-0 rotate-[-0.61] z-[-1]">
-          <Image
-            src="/assets/images/acedia-guide-(1)-15.png"
-            width={1850}
-            height={1545}
-          />
-        </div>
-        <div className="z-10">
-          <h2 className="text-[#A2FF00] text-[88px] leading-[86px] font-serif font-semibold underline underline-offset-[12px]">
-            Services
-          </h2>
-          <div className="mt-[100px] flex gap-[64px] font-serif">
-            <Image
-              className="rounded-[32px]"
-              src="/assets/images/services-section.jpg"
-              width={565}
-              height={622}
-            />
-            <div className="flex flex-col gap-[60px]">
-              <div>
-                <h3 className="font-bold text-[26px] leading-[32px] text-[#F7FCDE] mb-[15px]">
-                  Game Development
-                </h3>
-                <p className="font-medium text-[18px] leading-[36px] text-[#F7FCD] opacity-90 max-w-[540px]">
-                  With over two decades of experience, we're really good at
-                  building websites, marketing platforms, web applications, and
-                  mobile apps. We're currently using technologies like NextJS,
-                  Astro, React, and Svelte, among others, to build something
-                  modern and maintainable.
-                </p>
-              </div>
-              <div>
-                <h3 className="font-bold text-[26px] leading-[32px] text-[#F7FCDE] mb-[15px]">
-                  Game Development
-                </h3>
-                <p className="font-medium text-[18px] leading-[36px] text-[#F7FCD] opacity-90 max-w-[540px]">
-                  With over two decades of experience, we're really good at
-                  building websites, marketing platforms, web applications, and
-                  mobile apps. We're currently using technologies like NextJS,
-                  Astro, React, and Svelte, among others, to build something
-                  modern and maintainable.
-                </p>
-              </div>
-              <p className="font-bold text-[26px] leading-[32px] text-[#F7FCDE]">
-                Ready to make a change?{" "}
-                <span className="text-[#CFED4E] underline">
-                  Let's get started
-                </span>
-              </p>
-            </div>
-            <div className="flex flex-col gap-[60px]">
-              <div>
-                <h3 className="font-bold text-[26px] leading-[32px] text-[#F7FCDE] mb-[15px]">
-                  Game Development
-                </h3>
-                <p className="font-medium text-[18px] leading-[36px] text-[#F7FCD] opacity-90 max-w-[540px]">
-                  With over two decades of experience, we're really good at
-                  building websites, marketing platforms, web applications, and
-                  mobile apps. We're currently using technologies like NextJS,
-                  Astro, React, and Svelte, among others, to build something
-                  modern and maintainable.
-                </p>
-              </div>
-              <div>
-                <h3 className="font-bold text-[26px] leading-[32px] text-[#F7FCDE] mb-[15px]">
-                  Game Development
-                </h3>
-                <p className="font-medium text-[18px] leading-[36px] text-[#F7FCD] opacity-90 max-w-[540px]">
-                  With over two decades of experience, we're really good at
-                  building websites, marketing platforms, web applications, and
-                  mobile apps. We're currently using technologies like NextJS,
-                  Astro, React, and Svelte, among others, to build something
-                  modern and maintainable.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Services />
       <Footer />
     </>
   );
